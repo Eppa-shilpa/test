f8a154a230a533f1705e9a93a8e3d4f83f076ed3
EppaShilpa <113342175+EppaShilpa@users.noreply.github.com>
2024-07-01 17:32:12 +0530

    flipcart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FlipkartMock';
+  cities: any | undefined;
+
+  selectedCity1: any | undefined;
+  selectedCity2: any | undefined;
+
+  @ViewChild('ref1') ref1: any;
+  @ViewChild('ref2') ref2: any;
 
   ngOnInit() {
+    this.cities = [
+      { name: 'New York', code: 'NY' },
+      { name: 'Rome', code: 'RM' },
+      { name: 'London', code: 'LDN' },
+      { name: 'Istanbul', code: 'IST' },
+      { name: 'Paris', code: 'PRS' }
+    ];
+  }
+
+  parentClick() {
+    alert('alert');
+
+  }
+
+  childClick1(event: MouseEvent) {
+    this.ref2.hide();
+    event.stopPropagation();
+  }
+
+  childClick2(event: MouseEvent) {
+    this.ref1.hide();
+    event.stopPropagation();
   }
 }
